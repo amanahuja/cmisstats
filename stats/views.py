0c5b02317e4e51c65710577cc817631d2d1642da
Aman <amanahuja@gmail.com>
2011-07-12 16:26:48 -0700

    Rearranged views

--- stats/views.py
+++ stats/views.py
@@ -1,9 +1,15 @@
-from stats.models import RepoForm
+from stats.models import Repo, RepoForm
 from django.template import RequestContext
 from django.shortcuts import render_to_response
 
 def home(request):
-    
+    repos = Repo.objects.all()
+    return render_to_response('test.html', {
+        'repo_list': repos,
+        }) 
+        
+
+def addRepo (request):    
     if request.method == 'POST': 
         form = RepoForm(request.POST)
         if form.is_valid():
