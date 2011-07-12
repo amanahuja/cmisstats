29b1ffa333b104243f71388d4fa4a931ef50bba9
Aman <amanahuja@gmail.com>
2011-07-12 15:37:20 -0700

    Form validation basic and CSRF token

--- stats/views.py
+++ stats/views.py
@@ -1,9 +1,17 @@
 from stats.models import RepoForm
+from django.template import RequestContext
 from django.shortcuts import render_to_response
 
 def home(request):
-    form = RepoForm()
     
+    if request.method == 'POST': 
+        form = RepoForm(request.POST)
+        if form.is_valid():
+            form.save
+            
+    else:  
+        form = RepoForm()
+        
     return render_to_response("test.html", {
         "form": form, 
-        })
+        }, context_instance=RequestContext(request))
