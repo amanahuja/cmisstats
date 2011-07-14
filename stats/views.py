dee18169ef22c852e0a3d83272adacf82fd32ad2
Aman <amanahuja@gmail.com>
2011-07-14 16:43:51 -0700

    Create Ajax view, temporarily disable code for move to async task

--- stats/views.py
+++ stats/views.py
@@ -49,14 +49,26 @@ def browse(request, repo_id):
     
     ''' 
     # todo: We've already connected here and should use a persisted connection.
+    '''
     cmisserver = get_object_or_404(Repo, pk=repo_id)
     cmisrepo = CMISRepoConnector(repo_id)
     
     rootfolder = cmisrepo.getRootFolder()
     directory = [] 
     directory.append(parseFolder(rootfolder))
-        
+    '''
+     
     return render_to_response('browse.html', {
-            'rootfolder': rootfolder.name,
-            'directory': directory,
+            'rootfolder': "rootfolder.name",
+            'directory': "directory",
             })
+
+def ajax_directory_tree( request ):
+    if request.is_ajax():
+        results = Repo.objects.all()
+    else:
+        return HttpResponse('not an AJAX request.')
+        
+    return render_to_response( 'json_template_handler.html', 
+       {'results': results,},
+       context_instance = RequestContext( request ) )
