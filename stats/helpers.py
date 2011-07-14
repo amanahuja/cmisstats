64770b7fcf588eecdd3700b070e92d4673b5faf8
Aman <amanahuja@gmail.com>
2011-07-14 13:46:23 -0700

    Created a recursive function to fetch documents and subfolders

--- stats/helpers.py
+++ stats/helpers.py
@@ -22,3 +22,18 @@ def CMISRepoConnector(user, repo_id = 1):
         raise Http404('\nError in utils.CMISRepoConnector(): Unable to connect to repository.')
 
     return crepo
+
+def parseFolder(folder):
+    #name = folder.getProperties()['cmis:name']
+    children = folder.getChildren().getResults()
+    
+    if len(children) == 0: 
+        return folder.name,
+    else: 
+        sublist = []
+        for object in children: 
+            if type(object).__name__ == 'Folder': 
+                sublist.append(parseFolder(object))
+            else: 
+                sublist.append(object.name)
+        return (folder.name, sublist)
